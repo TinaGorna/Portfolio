3ce4044c92ab94db623c024cdb4aafdbbe8b99c6
TinaGorna <kristina.gorbunova1997@gmail.com>
2021-05-11 15:43:32 +0300

    added new blog to BlogsPage in Linkedin

--- src/Components/allBlogs.js
+++ src/Components/allBlogs.js
@@ -1,5 +1,6 @@
 import blog from '../img/portImages/blog.jpg';
 import lang from '../img/portImages/lang.jpg';
+import context from '../img/portImages/ContextAPI.jpg';
 const blogs = [
     {
         id: 1,
@@ -15,6 +16,15 @@ const blogs = [
         image: lang,
         title: 'How to learn languages',
         link: 'https://www.instagram.com/p/CLXfGM2HMQx/',
+        date : '09',
+        month: 'Dec',
+        year: '2019'
+    },
+    {
+        id: 3,
+        image: context,
+        title: 'Context API: how to use',
+        link: 'https://www.linkedin.com/pulse/react-context-%D1%87%D1%82%D0%BE-%D1%82%D0%B0%D0%BA%D0%BE%D0%B5-%D0%B8-%D0%BA%D0%B0%D0%BA-%D0%B5%D0%B3%D0%BE-%D0%B8%D1%81%D0%BF%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D1%8C-kristina-gorbunova',
         date : '09',
         month: 'Dec',
         year: '2019'
